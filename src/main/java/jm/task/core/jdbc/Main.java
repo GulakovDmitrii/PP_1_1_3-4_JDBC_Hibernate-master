df34b7e05808a4ea374234994060b7994109ceba
GulakovDmitrii <gulakov127@gmail.com>
2024-06-30 19:55:06 +0300

    v4Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,16 +5,24 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-    UserServiceImpl userService = new UserServiceImpl();
-    userService.dropUsersTable();
-    userService.createUsersTable();
-    userService.saveUser("Егор", "Иванов", (byte) 25);
-    userService.saveUser("Сергей", "Петров", (byte) 35);
-    userService.saveUser("Максим", "Федоров", (byte) 45);
-    userService.saveUser("Иван", "Васильев", (byte) 55);
-    userService.getAllUsers().forEach(System.out::println);
-    userService.cleanUsersTable();
-    userService.dropUsersTable();
-        Util.closeConnection();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Егор", "Иванов", (byte) 25);
+        userService.saveUser("Сергей", "Петров", (byte) 35);
+        userService.saveUser("Максим", "Федоров", (byte) 45);
+        userService.saveUser("Иван", "Васильев", (byte) 55);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.dropUsersTable();
+//    UserServiceImpl userService = new UserServiceImpl();
+//    userService.dropUsersTable();
+//    userService.createUsersTable();
+//    userService.saveUser("Егор", "Иванов", (byte) 25);
+//    userService.saveUser("Сергей", "Петров", (byte) 35);
+//    userService.saveUser("Максим", "Федоров", (byte) 45);
+//    userService.saveUser("Иван", "Васильев", (byte) 55);
+//    userService.getAllUsers().forEach(System.out::println);
+//    userService.cleanUsersTable();
+//    userService.dropUsersTable();
+//        Util.closeConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -13,7 +21,36 @@ public class Util {
     private static final String PASSWORD = "Monitor4286";
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory;
+
     private static Connection connection = null;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                System.out.println("Соединение с базой данных прошло успешно");
+            } catch (Exception e) {
+                System.out.println("Нет соединения с базой данных");
+                e.printStackTrace();
+            }
+        }
+
+        return sessionFactory;
+    }
 
     public static Connection getConnection()  {
 
