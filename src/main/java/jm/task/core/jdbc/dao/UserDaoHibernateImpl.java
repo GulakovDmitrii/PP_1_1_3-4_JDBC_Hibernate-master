6a476ff63e42bac1e03dab5a5af088547e1ed3dc
GulakovDmitrii <gulakov127@gmail.com>
2024-07-23 17:59:01 +0300

    v5rHibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
 
     private final SessionFactory sessionf = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
